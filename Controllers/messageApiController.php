e266d90da57d9c33c5bd6e8edfdddba61d85d5ae
cjdsnwxy <cjdsnwxy@163.com>
2016-04-18 18:49:55 +0800

    messageApiController

---
+++ Controllers/messageApiController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 16-4-18
+ * Time: 下午4:44
+ */
+
+include $_SERVER['DOCUMENT_ROOT'].'/Controllers/Controller.php';
+class messageApiController extends Controller
+{
+    //发送信息
+    public function actionSendMsg(){
+
+    }
+
+    //获得收到信息列表
+    public function actionReceiveMsgList(){
+
+    }
+
+    //获得发送的信息列表
+    public function actionSendMsgList(){
+
+    }
+}
