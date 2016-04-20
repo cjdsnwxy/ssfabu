ef2bb07f15fd743f3f82799ee88be4809fa995de
cjdsnwxy <cjdsnwxy@163.com>
2016-04-20 19:06:02 +0800

    indaxPage 和 page.js

--- Views/Index/index.php
+++ Views/Index/index.php
@@ -22,14 +22,14 @@
   </div>
   <div class="weui_text_area">
     <h1 class="weui_msg_title">神速发布系统</h1>
-    <p class="weui_msg_desc">告诉我你想做什么</p>
+    <p class="weui_msg_desc">welcome to ssfabu</p>
   </div>
   <div class="weui_opr_area">
     <p class="weui_btn_area">
       <a id="joinGroupBtn" class="weui_btn weui_btn_primary">加入群组</a>
       <a id="myGroupBtn" class="weui_btn weui_btn_primary">我加入的</a>
       <a id="myCreateBtn" class="weui_btn weui_btn_primary">我创建的</a>
-      <a id="createGrouoBtn" class="weui_btn weui_btn_primary">创建群组</a>
+      <a id="createGroupBtn" class="weui_btn weui_btn_primary">创建群组</a>
     </p>
   </div>
   <div class="weui_extra_area">
@@ -37,23 +37,67 @@
   </div>
 </div>
 <div id="joinGroupPage" style="display: none;">
-  加入群组
+  <div id="indexPage" class="weui_msg">
+    <div class="weui_icon_area">
+      <img style="color:#04BE02" src="./Views/Public/img/icon.png">
+    </div>
+    <div class="weui_text_area">
+      <h1 class="weui_msg_title">神速发布系统</h1>
+      <p class="weui_msg_desc">加入群组</p>
+    </div>
+    <div class="weui_cells weui_cells_form">
+      <div class="weui_cell">
+        <div class="weui_cell_hd"><label class="weui_label">群组ID</label></div>
+        <div class="weui_cell_bd weui_cell_primary">
+          <input class="weui_input" type="tel" placeholder="请输入群组ID">
+        </div>
+      </div>
+    </div>
+    <div class="weui_opr_area">
+      <p class="weui_btn_area">
+        <a href="javascript:;" class="weui_btn weui_btn_primary">确定</a>
+        <a href="#" onclick="showIndexPage()" class="weui_btn weui_btn_default">返回首页</a>
+      </p>
+    </div>
+    <div class="weui_extra_area">
+      <a href="">使用帮助</a>
+    </div>
+  </div>
 </div>
 <div id="myGroupPage" style="display: none;">
-  我加入的
+  我加入的ddd
 </div>
 <div id="myCreatePage"style="display: none;" >
   我创建的
 </div>
 <div id="createGroupPage" style="display: none;">
-  <div class="weui_cells weui_cells_form">
-    <div class="weui_cell">
-      <div class="weui_cell_hd"><label class="weui_label">时间</label></div>
-      <div class="weui_cell_bd weui_cell_primary">
-        <input id='datetime-picker' type="text" class="weui_input" placeholder="请输入时间">
+  <div id="indexPage" class="weui_msg">
+    <div class="weui_icon_area">
+      <img style="color:#04BE02" src="./Views/Public/img/icon.png">
+    </div>
+    <div class="weui_text_area">
+      <h1 class="weui_msg_title">神速发布系统</h1>
+      <p class="weui_msg_desc">告诉我你想做什么</p>
+    </div>
+    <div class="weui_cells weui_cells_form">
+      <div class="weui_cell">
+        <div class="weui_cell_hd"><label class="weui_label">时间</label></div>
+        <div class="weui_cell_bd weui_cell_primary">
+          <input id='datetime-picker' type="text" class="weui_input" placeholder="请输入时间">
+        </div>
       </div>
     </div>
+    <div class="weui_opr_area">
+      <p class="weui_btn_area">
+        <a href="javascript:;" class="weui_btn weui_btn_primary">确定</a>
+        <a href="#" onclick="showIndexPage()" class="weui_btn weui_btn_default">返回首页</a>
+      </p>
+    </div>
+    <div class="weui_extra_area">
+      <a href="">使用帮助</a>
+    </div>
   </div>
+</div>
   <script src="http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
   <script src="./Views/Public/js/jquery-weui.js"></script>
   <script src="./Views/Public/js/page.js"></script>
--- Views/Public/js/page.js
+++ Views/Public/js/page.js
@@ -1,19 +1,67 @@
 $(document).ready(function(){
+
+  $("#showIndexBtn").click(function (){
+    showIndexPage();
+  });
+
   $("#joinGroupBtn").click(function(){
-    $("#indexPage").hide();
-    $("#joinGroupPage").show();
+    showJoinGroupPage();
   });
+
   $("#myGroupBtn").click(function(){
-    $("#indexPage").hide();
-    $("#myGroupPage").show();
+    showMyGroupPage();
   });
+
   $("#myCreateBtn").click(function(){
-    $("#indexPage").hide();
-    $("#myCreatePage").show();
+    showMyCreatePage();
   });
-  $("#createGrouoBtn").click(function(){
-    $("#indexPage").hide();
-    $("#createGroupPage").show();
+
+  $("#createGroupBtn").click(function(){
+    showCreateGroupPage();
   });
+
   $("#datetime-picker").datetimePicker();
+
+
 });
+
+function showIndexPage(){
+  $("#joinGroupPage").hide();
+  $("#myGroupPage").hide();
+  $("#myCreatePage").hide();
+  $("#createGroupPage").hide();
+  $("#indexPage").show();
+}
+
+function showJoinGroupPage(){
+  $("#indexPage").hide();
+  $("#myGroupPage").hide();
+  $("#myCreatePage").hide();
+  $("#createGroupPage").hide();
+  $("#joinGroupPage").show();
+}
+
+function showMyGroupPage(){
+  $("#joinGroupPage").hide();
+  $("#indexPage").hide();
+  $("#myCreatePage").hide();
+  $("#createGroupPage").hide();
+  $("#myGroupPage").show();
+}
+
+function showMyCreatePage(){
+  $("#joinGroupPage").hide();
+  $("#myGroupPage").hide();
+  $("#indexPage").hide();
+  $("#createGroupPage").hide();
+  $("#myCreatePage").show();
+}
+
+function showCreateGroupPage(){
+  $("#joinGroupPage").hide();
+  $("#myGroupPage").hide();
+  $("#myCreatePage").hide();
+  $("#indexPage").hide();
+  $("#createGroupPage").show();
+}
+
