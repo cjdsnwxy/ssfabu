f451ece3677561fce31e9934187902c93e4267cd
cjdsnwxy <cjdsnwxy@163.com>
2016-04-21 19:15:28 +0800

    二维码生成函数

--- Controllers/indexController.php
+++ Controllers/indexController.php
@@ -12,4 +12,11 @@ class indexController extends Controller
     public function actionIndex(){
         $this->display('Index/index');
     }
+
+    public function actionQrcode(){
+        include $_SERVER['DOCUMENT_ROOT'].'/Ext/phpqrcode.php';
+        QRcode::png('http://www.baidu.com/');
+        //http://127.0.0.1/index.php?c=index&a=qrcode
+
+    }
 }
---
+++ ewm.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 16-4-21
+ * Time: 下午6:42
+ */
+?>
+<img src="http://127.0.0.1/index.php?c=index&a=qrcode">
