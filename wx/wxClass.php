960f8026b3af93040cb01be658775d6b6523ad1d
cjdsnwxy <cjdsnwxy@163.com>
2016-05-03 17:13:33 +0800

    wxclass demo

--- wx/wxClass.php
+++ wx/wxClass.php
@@ -30,8 +30,6 @@ class wxClass{
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
         curl_setopt($curl, CURLOPT_URL, $url);
         $res = curl_exec($curl);
         curl_close($curl);
