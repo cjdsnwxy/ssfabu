f07edb837d84807eb52a8baff1ed4275a6598c9c
cjdsnwxy <cjdsnwxy@163.com>
2016-05-03 16:47:13 +0800

    wxclass demo

---
+++ wx/index.php
@@ -0,0 +1,10 @@
+<?php
+$code = $_POST['code'];
+echo "code->".$code;
+echo "<br>";
+include 'wxClass.php';
+$appId = 'wxa54b997e82462e5a';
+$appSecret = '2b9c188883ebf39f9203eaee4876dda5';
+$class = new wxClass($appId,$appSecret,$code);
+$openId = $class->getOpenId;
+echo "openId->".$openId;
