3b4863a73a8451b169057492c1934b9e1382ae1b
cjdsnwxy <cjdsnwxy@163.com>
2016-05-04 22:23:00 +0800

    test getToken

---
+++ textWx.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: King
+ * Date: 2016/5/4
+ * Time: 22:21
+ */
+
+include 'Ext/getAccessTokenClass.php';
+$class = new getAccessTokenClass('wxa54b997e82462e5a','2b9c188883ebf39f9203eaee4876dda5');
+$a = $class->getToken();
+var_dump($a);
